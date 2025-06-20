e3b3d9af47e853b02cf4a54c19357590b394e8a9
sidgureja7803 <siddhantgureja39@gmail.com>
2025-06-21 05:23:28 +0530

    Final changes done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
 // src/config.js or wherever you define constants
-export const BASE_URL =
-  import.meta.env.MODE === "production"
-    ? "https://dev-tinder-server.onrender.com"
-    : "https://localhost:7777";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:7777";
