e6e7d964478197deb3fa35f9c58380477580599f
sidgureja7803 <siddhantgureja39@gmail.com>
2025-06-21 03:56:02 +0530

    Frontend done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "https:localhost:7777";
+// src/config.js or wherever you define constants
+export const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? "https://dev-tinder-server.onrender.com"
+    : "https://localhost:7777";
